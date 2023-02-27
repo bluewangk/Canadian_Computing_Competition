c9958e6bccc2781a73b3568dc014b529674b138b
Kai Wang <bluewangk@gmail.com>
2023-02-27 11:16:27 -0500

    Add problem 2

---
+++ 2023/junior/problem_2.py
@@ -0,0 +1,23 @@
+pepper_SHU_dict = {
+    "Poblano":  1500,
+    "Mirasol":  6000,
+    "Serrano":  15500,
+    "Cayenne":  40000,
+    "Thai":     75000,
+    "Habanero": 125000,
+}
+
+def calculate_total_speciness(peppers):
+    total_speciness = 0
+    for pepper in peppers:
+        total_speciness += pepper_SHU_dict[pepper]
+    
+    return total_speciness
+
+if __name__ == '__main__':
+    number = input()
+    peppers = []
+    for _ in range(int(number)):
+        peppers.append(input())
+
+    print(calculate_total_speciness(peppers))
---
+++ 2023/junior/problem_2_test.py
@@ -0,0 +1,4 @@
+from problem_2 import calculate_total_speciness
+
+def test_case():
+    assert calculate_total_speciness(["Poblano", "Cayenne", "Thai", "Poblano"]) == 118000
