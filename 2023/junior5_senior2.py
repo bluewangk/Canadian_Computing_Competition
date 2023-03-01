fa3562db93b66d4304b481affac87c40dba169a5
Kai Wang <bluewangk@gmail.com>
2023-02-28 21:52:29 -0600

    A draft

--- 2023/junior5_senior2.py
+++ 2023/junior5_senior2.py
@@ -1,27 +1,11 @@
-def hunt_word(word, letters_grid):
-    letters_grid = [line.replace(" ", "") for line in letters_grid]
-    # TODO
+def design_palindromic_poster(N, M, R, C):
+    if R >= N or C >= M:
+        return "IMPOSSIBLE"
     
-    
+    words = ["a" * M for _ in range(N)]
+    return "\n".join(words)
 
 
 if __name__ == '__main__':
-    word = input()
-    rows= int(input())
-    columns = int(input())
-    letters_grid = []
-    for row in range(rows):
-        letters_grid.append(input())
-
-    print(hunt_word(word, letters_grid))
-
-    # word = "NATURE"
-    # grid = [
-    #     "N A T S F E G Q N",
-    #     "S A I B M R H F A",
-    #     "C F T J C U C L T",
-    #     "K B H U P T A N U",
-    #     "D P R R R J D I R",
-    #     "I E E K M E G B E",
-    # ]
-    # print(hunt_word(word, grid))
+    [N, M, R, C] = [int(n) for n in input().split()]
+    print(design_palindromic_poster(N, M, R, C))
